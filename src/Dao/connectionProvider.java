ba399ce4630dca84f0eff1016c49e3888716f138
asif-ethmoid <asifrahman981@gmail.com>
2022-08-17 02:34:00 +0600

    UpdatedLoginPage

--- src/Dao/connectionProvider.java
+++ src/Dao/connectionProvider.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package Dao;
-
+import java.sql.*;
 /**
  *
  * @author AsifRahman
  */
 public class connectionProvider {
+    public static Connection getCon(){
+        try{
+          class.forName("com.mysql.jdbc.Driver");
+          Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306");
+        }
+            catch(Exception e){
+                return null;
+            }
+    }
     
 }
